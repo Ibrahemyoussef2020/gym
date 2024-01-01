cd83f8bde7c3e3f95863f084526e617271bd39b4
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-01-01 16:14:11 +0200

    add context api to select logger and his validities

---
+++ src/contextApi/RememberMeContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const RememberMeContext = createContext()
+
+export default RememberMeContext
--- src/contextApi/index.js
+++ src/contextApi/index.js
@@ -1,1 +1,2 @@
 export {default as ValidityContext } from "./ValidityContext"
+export {default as RememberMeContext } from "./RememberMeContext"
