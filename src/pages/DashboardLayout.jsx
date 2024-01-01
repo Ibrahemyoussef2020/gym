e3ecbbc3a6601052b7e1ffc781f722cc22d5d380
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-01-01 16:49:42 +0200

    make changes cous of mainDash rename and change a default root and use react location to select the active dashboard page

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router'
+import { Outlet} from 'react-router'
 import {DashboardHeader,DashboardAside} from '../layout'
 
 const DashboardLayout = () => {
@@ -6,7 +6,7 @@ const DashboardLayout = () => {
   return (
     <section className='px-10 py-5 flex max-w-[100vw] overflow-auto'>   
           <DashboardAside/>
-          <div className='flex-1 p-5 min-w-[500px] lg:min-w-[700px] bg-costum-clr_dark_white'>
+          <div className='dashboard-layout flex-1 p-5 min-w-[700px] bg-costum-clr_dark_white'>
             <DashboardHeader />
             <Outlet/> 
           </div>      
