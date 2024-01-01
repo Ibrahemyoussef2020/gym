ad50527bf79a3a05aa923c094c9dff9f864f31e8
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-01-01 20:23:37 +0200

    change name project and api link to render link and configure to hosting

--- src/index.js
+++ src/index.js
@@ -2,62 +2,16 @@ import React from 'react';
 import {createBrowserRouter , RouterProvider} from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import {QueryClient , QueryClientProvider } from 'react-query'
+
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
 import reportWebVitals from './reportWebVitals';
 import './index.css'
 
+
 import App from './App';
 
-/*
-const router = createBrowserRouter([
-  {
-    path:'/home',
-    element:<HomePage />
-  },
-  {
-    path:'dashboard-layout',
-    element:<DashboardLayout />,
-    children:[
-      {
-        path:'dashboard',
-        element:<Dashboard />,
-        index:true
-      },
-      {
-        path:'admin-profile',
-        element:<Admin_Profile />,
-      },
-      {
-        path:'plan',
-        element:<Plan />
-      },
-      {
-        path:'inventory',
-        element:<Inventory />
-      },
-      {
-        path:'view-members',
-        element:<View_Members />
-      },
-      {
-        path:'coaches',
-        element:<Coaches />
-      },
-      {
-        path:'report',
-        element:<Report />
-      }
-    ]
-  },
-  {
-    path:'sign-in',
-    element:<SignIn />
-  },
-  {
-    path:'/',
-    element:<SignUp />
-  }
-])
-*/
+if(process.env.NODE_ENV ===  'production') disableReactDevTools()
 
 const queryClient = new QueryClient()
 
