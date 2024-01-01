80f3df4afc1975c5af6e9a2093b5c60aeb10fbfc
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-01-01 16:22:44 +0200

    add many ways to show date we need it

--- src/utilities/selectDates.js
+++ src/utilities/selectDates.js
@@ -1,13 +1,5 @@
 
-export const selectDate = (num = 1) => {
-    const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() + num)
-
-  return  currentDate.toDateString()
-  
-}
-// need to modify
-export const selectNumaricDate = (num = 1) => {
+export const selectDate = (num = 1,date) => {
     const currentDate = new Date();
     currentDate.setDate(currentDate.getDate() + num)
 
@@ -15,18 +7,59 @@ export const selectNumaricDate = (num = 1) => {
   
 }
 
-export const selectDateMonth = (num) => {
-    const currentDate = new Date(num);
-    const month = currentDate.toLocaleString('default', { month: 'long' })
-
-    return month
+export const selectNumaricDate = (num = 1,date) => {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + num)
+  return  currentDate.toDateString()
   
 }
-// need to modify
-export const selectDateToMonth = (num) => {
-    const currentDate = new Date(num);
-    const month = currentDate.toLocaleString('default', { month: 'long' })
 
-    return month
-  
+
+export const selectDateMonth = (date,limit) => {
+    const startDate = new Date(date);
+    const startMonth = startDate.toLocaleString('fr-FR', { month: 'long' })
+
+
+    const endDate = startDate.setMonth(startDate.getMonth()+ limit);
+    const endMonthInNumber = new Date(endDate);
+    const endMonth = endMonthInNumber.toLocaleString('fr-FR', { month: 'long' })
+
+    const returnedDate = {
+        startDate:startMonth,
+        endDate:endMonth
+    }
+
+    return returnedDate
 }
+
+
+export const selectDateToMonth = (date,limit) => {
+  const startDate = new Date(date);
+  const startMonth = startDate.toLocaleString('fr-FR', { month: 'long' })
+  
+
+  const startAndEndDay = startDate.getDay()
+
+
+  const endDate = startDate.setMonth(startDate.getMonth()+ limit);
+  const endMonthInNumber = new Date(endDate);
+  const endMonth = endMonthInNumber.toLocaleString('fr-FR', { month: 'long' })
+
+  const returnedDate = {
+      startDate:`${startMonth[0].toUpperCase()}${startMonth.slice(1,3)} ${startAndEndDay}`,
+      endDate:`${endMonth[0].toUpperCase()}${endMonth.slice(1,3)} ${startAndEndDay}`
+  }
+
+  return returnedDate
+}
+
+
+export const selectMonthInEnglish = (date)=>{
+  const fullDate = new Date(date);
+  const month = fullDate.toLocaleString('fr-FR', { month: 'long' })
+  
+  return `${month[0]?.toUpperCase()}${month?.slice(1)}`
+}
+
+
+
