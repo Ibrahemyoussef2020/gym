8515da9b12edf7f363edceb346afe4bcf8ee2b5f
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-01-01 16:37:24 +0200

    no real change happend yet

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
 import axios   from "axios";
 
+
 const api = axios.create({
-    baseURL : 'http://localhost:4000'
+    baseURL : 'http://localhost:4000/'
+
 })
 
 
@@ -11,7 +13,7 @@ export const getData = async (seletedData)=> await api.get(seletedData)
 export const getSelectedData = async (seletedData,id)=> await api.get(`${seletedData}/${id}`)
 .then(res => res.data)
 
-export const addData = async (seletedData,{...added})=> await api.post(`${seletedData}/`,added)
+export const addData = async ({seletedData,...added})=> await api.post(`${seletedData}`,added)
 .then(res => res.data)
 
 export const modifyData = async ({seletedData,id,...modified})=> await api.put(`${seletedData}/${id}`,modified)
