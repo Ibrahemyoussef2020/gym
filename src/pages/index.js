f1b3ed034cba87afdd333b380760fc71a5412f93
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-01-01 16:12:27 +0200

    change mainDashboard link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export {default as HomePage} from './HomePage'
 
 export {default as DashboardLayout} from './DashboardLayout'
-export {default as Dashboard} from './Dashboard'
+export {default as MainDash} from './MainDash'
 export {default as Admin_Profile} from './Admin_Profile'
 export {default as Registration} from './Registration'
 export {default as Plan} from './Plan'
